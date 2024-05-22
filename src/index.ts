0d8a0be7b9b611c7ed31c5f74311528e9b396485
Eduardo1060 <eduardo.demetrio2000@gmail.com>
2024-05-22 15:07:14 -0300

    segundo commit

--- src/index.ts
+++ src/index.ts
@@ -25,3 +25,61 @@ class Pessoa { // classe
 let pessoa1 = new Pessoa("Eduardo", 20);
 pessoa1.andar(); // Pessoa andando
 console.log(pessoa1.nome); // Eduardo
+
+
+// Encapsulamento:
+
+// O encapsulamento é o conceito de esconder os detalhes de implementação de um objeto e exibir apenas as informações necessárias.
+// Em TypeScript, podemos usar modificadores de acesso como public, private e protected para controlar o acesso aos membros de uma classe.
+
+    class ContaBancaria { 
+        private saldo: number;
+
+        constructor(saldoInicial: number) {
+            this.saldo = saldoInicial;
+        }
+
+        depositar(valor: number) {
+            this.saldo += valor;
+        }
+
+        sacar(valor: number) {
+            if (valor <= this.saldo) {
+                this.saldo -= valor;
+                console.log(`Saque de ${valor} realizado. Saldo restante: ${this.saldo}`);
+            } else {
+                console.log("Saldo insuficiente.");
+            }
+        }
+    }
+
+    let minhaConta = new ContaBancaria(1000);
+    minhaConta.depositar(500);
+    minhaConta.sacar(200);
+
+//     Herança:
+
+// Herança é um mecanismo que permite que uma classe herde propriedades e métodos de outra classe.
+// A classe que herda é chamada de classe filha (ou subclasse), e a classe da qual ela herda é chamada de classe pai (ou superclasse).
+
+class Animal {
+    nome: string;
+
+    constructor(nome: string) {
+        this.nome = nome;
+    }
+
+    fazerBarulho() {
+        console.log("Barulho genérico de animal");
+    }
+}
+
+class Cachorro extends Animal {
+    fazerBarulho() {
+        console.log("Au Au!");
+    }
+}
+
+let meuCachorro = new Cachorro("Rex");
+console.log(meuCachorro.nome); // Saída: Rex
+meuCachorro.fazerBarulho(); // Saída: Au Au!
